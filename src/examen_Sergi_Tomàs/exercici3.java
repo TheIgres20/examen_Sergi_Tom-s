bc2d70c1b5f1c884494abbae35e8f2e9146ea998
Sergi Tomàs <Sergi Toms@192.168.1.36>
2018-02-27 09:39:37 +0100

    Examen Sergi Tomàs

---
+++ src/examen_Sergi_Tomàs/exercici3.java
@@ -0,0 +1,21 @@
+package examen_Sergi_Tomàs;
+
+public class exercici3 
+{
+	public static void main (String [] args)
+	{
+		printOne ();
+		printOne ();
+		printTwo ();
+	}
+	public static void printOne ()
+	{
+		System.out.println("Hello World");
+	}
+	
+	public static void printTwo ()
+	{
+		printOne ();
+		printOne ();
+	}
+}
